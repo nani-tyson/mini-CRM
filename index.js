4b1fc22d3775ee7b5795094a4ca23de441507d40
nani-tyson <nanityson10@gmail.com>
2025-09-10 22:01:40 +0000

    backend setup

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+//configs
+import connectDB from './config/db.js';
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, async () => {
+  connectDB(process.env.MONGO_URI);
+  console.log(`Server is running on port ${PORT}`);
+});
